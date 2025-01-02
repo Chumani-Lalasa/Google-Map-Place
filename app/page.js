f3912cb2bd7f9d73eb1af929886275af3a9b352a
Chumani Lalasa <chumanilalasa@gmail.com>
2025-01-02 15:31:18 +0530

    authentication added

--- app/page.js
+++ app/page.js
@@ -1,9 +1,21 @@
+"use client";
+import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const {data: session} = useSession();
+  const router = useRouter();
+  useEffect(() => { 
+    if(!session?.user){
+      router.push('/login')
+    }
+  }, []);
   return (
    <div>
       <h1>Hello World</h1>
+      <button onClick={()=>signOut()}>Sign Out</button>
    </div>
   );
 }
