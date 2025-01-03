ac03811c617677d1527975c3ca893c7ad5ff5285
Chumani Lalasa <chumanilalasa@gmail.com>
2025-01-03 17:43:56 +0530

    location tracked

--- components/Home/GoogleMapView.js
+++ components/Home/GoogleMapView.js
@@ -1,23 +1,37 @@
-import { GoogleMap, LoadScript } from '@react-google-maps/api'
-import React from 'react'
+import { UserLocationContext } from '@/context/UserLocationContext'
+import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api'
+import React, { useContext } from 'react'
 
 function GoogleMapView() {
+    const {userLocation, setUserLocation} = useContext(UserLocationContext)
     const containerStyle = {
         width: '100%',
         height: '500px'
     }
     const cordinate = {lat: -34.397, lng: 150.644}
+    console.log(userLocation);
   return (
     <div>
       <LoadScript
         googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
+        mapIds={['d98ffb2796a877c0']}
       >
         <GoogleMap
             mapContainerStyle={containerStyle}
-            center={cordinate}
+            center={userLocation}
+            options={{mapId: 'd98ffb2796a877c0'}}
             zoom={12}
         >
-
+            <MarkerF 
+                position={userLocation}
+                icon={{
+                    url: 'user-location.png',
+                    scaledSize: {
+                        width: 50,
+                        height: 50
+                    }
+                }}
+            />
         </GoogleMap>
       </LoadScript>
     </div>
